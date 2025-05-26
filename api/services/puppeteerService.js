c79c39c883d337fdaf98ab792f4345f0a118280a
cyberrrnaut <cyberrrnaut@gmail.com>
2025-05-26 20:22:04 +0530

    changed some dockerfiles

--- api/services/puppeteerService.js
+++ api/services/puppeteerService.js
@@ -32,15 +32,16 @@ async function automateLoginAndScrape(pan, password, maxRetries = 3) {
         const browser = await puppeteer.launch({
             headless: true,
             args: [
-              '--no-sandbox',
-              '--disable-setuid-sandbox',
-              '--disable-dev-shm-usage',
-              '--disable-accelerated-2d-canvas',
-              '--no-first-run',
-              '--no-zygote',
-              '--single-process',
-              '--disable-gpu'
-            ]
+                '--no-sandbox',
+                '--disable-setuid-sandbox',
+                '--disable-dev-shm-usage',
+                '--disable-accelerated-2d-canvas',
+                '--no-first-run',
+                '--no-zygote',
+               '--single-process',
+                '--disable-gpu'
+              ]
+              
           });
           
           
